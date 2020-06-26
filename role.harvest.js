88a7f630b84577ff063c87b89ebae35faa9141f5
TheLowtech <novar34@Gmail.com>
2020-06-26 06:04:00 -0500

    Update role.harvest.js
    Added Tower to harvester refill type

--- role.harvest.js
+++ role.harvest.js
@@ -20,7 +20,9 @@ var roleHarvest = {
         if(creep.memory.target == undefined) {
             var sources = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                    return (structure.structureType == STRUCTURE_EXTENSION || 
+                            structure.structureType == STRUCTURE_SPAWN || 
+                            structure.structureType == STRUCTURE_TOWER) &&
                             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
 
