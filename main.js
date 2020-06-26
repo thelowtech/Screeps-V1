f77d9256c637c5ae12d94b0b842015894104d8f8
TheLowtech <novar34@Gmail.com>
2020-06-26 08:27:08 -0500

    Update main.js
    Moved desired number of creeps per job to room memory

--- main.js
+++ main.js
@@ -17,6 +17,40 @@ module.exports.loop = function() {
         }
     }
 
+    _.forEach(Game.rooms, function(room) {
+        if(room && room.controller && room.controller.my) {
+            // room loop
+            let minNumberOfHarvest = _.get(room.memory, ['cencus', 'harvest'], 1);
+            let minNumberOfBuild = _.get(room.memory, ['cencus', 'build'], 1);
+            let minNumberOfUpgrade = _.get(room.memory, ['cencus', 'upgrade'], 1);
+            let minNumberOfRepair = _.get(room.memory, ['cencus', 'repair'], 1);
+
+            var numberOfHarvest = _.sum(Game.creeps, (creep) => creep.memory.role == 'harvest');
+            var numberOfBuild = _.sum(Game.creeps, (creep) => creep.memory.role == 'build');
+            var numberOfUpgrade = _.sum(Game.creeps, (creep) => creep.memory.role == 'upgrade');
+            var numberOfRepair = _.sum(Game.creeps, (creep) => creep.memory.role == 'repair');
+
+            var name = -1;
+
+            if(numberOfHarvest < minNumberOfHarvest) {
+                name=Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE],undefined, {role: 'harvest', working:false, target: undefined});
+            }
+            else if(name == -1 && numberOfUpgrade < minNumberOfUpgrade) {
+                name=Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE],undefined, {role: 'upgrade', working:false, target: undefined});
+            }
+            else if(name == -1 && numberOfBuild < minNumberOfBuild && creep.room.find(FIND_CONSTRUCTION_SITES).length > 0) {
+                name=Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE],undefined, {role: 'build', working:false, target: undefined});
+            }
+            else if(name == -1 && numberOfRepair < minNumberOfRepair) {
+                name=Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE],undefined, {role: 'repair', working:false, target: undefined});
+            }
+        
+            if (!(name < 0)) {
+                console.log("Spawned new " + creep.memory.role + " creep: " + name);
+            }
+        }
+    })
+
     defenseTower.run();
 
     for(var name in Game.creeps) {
@@ -33,34 +67,5 @@ module.exports.loop = function() {
         if(creep.memory.role == 'repair') {
             roleRepair.run(creep);
         }
-    }
-
-    var name = -1;
-
-    var minNumberOfHarvest = 3;
-    var minNumberOfBuild = 2;
-    var minNumberOfUpgrade = 2;
-    var minNumberOfRepair = 1;
-
-    var numberOfHarvest = _.sum(Game.creeps, (c) => c.memory.role == 'harvest');
-    var numberOfBuild = _.sum(Game.creeps, (c) => c.memory.role == 'build');
-    var numberOfUpgrade = _.sum(Game.creeps, (c) => c.memory.role == 'upgrade');
-    var numberOfRepair = _.sum(Game.creeps, (c) => c.memory.role == 'repair');
-
-    if(numberOfHarvest < minNumberOfHarvest) {
-        name=Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE],undefined, {role: 'harvest', working:false, target: undefined});
-    }
-    else if(name == -1 && numberOfUpgrade < minNumberOfUpgrade) {
-        name=Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE],undefined, {role: 'upgrade', working:false, target: undefined});
-    }
-    else if(name == -1 && numberOfBuild < minNumberOfBuild && creep.room.find(FIND_CONSTRUCTION_SITES).length > 0) {
-        name=Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE],undefined, {role: 'build', working:false, target: undefined});
-    }
-    else if(name == -1 && numberOfRepair < minNumberOfRepair) {
-        name=Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE],undefined, {role: 'repair', working:false, target: undefined});
-    }
-
-    if (!(name < 0)) {
-        console.log("Spawned new " + creep.memory.role + " creep: " + name);
     }
 }
